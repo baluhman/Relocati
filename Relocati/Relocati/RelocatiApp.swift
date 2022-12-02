2f64f9acb9058f24bf089668035720f2e259d273
Oleg Krasnov <kelebra79@gmail.com>
2022-12-02 18:21:00 +0400

    create project

---
+++ Relocati/Relocati/RelocatiApp.swift
@@ -0,0 +1,17 @@
+//
+//  RelocatiApp.swift
+//  Relocati
+//
+//  Created by Oleg Krasnov on 02/12/2022.
+//
+
+import SwiftUI
+
+@main
+struct RelocatiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
